256e203148830365ea8a3cdf05bb164e0b66153f
ハルダルスティーブエヴァン <190180xd@yse-c.net>
2021-01-29 10:25:22 +0900

    first test

---
+++ app/Models/item.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $table = 'items';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'price',
+        'code',
+        'amount',
+    ];
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+}
